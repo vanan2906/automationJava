0b24c7c84ba65eb6caf24e176f686864021e743e
anhntv4 <anhntv4@smartosc.com>
2023-11-17 17:35:42 +0700

    m

--- Autotraining/src/main/java/Baitapjava/Baitap4.java
+++ Autotraining/src/main/java/Baitapjava/Baitap4.java
@@ -2,6 +2,8 @@ package Baitapjava;
 
 public class Baitap4 {
     public static void main(String[] args) {
+        //arning: in the working copy of 'Autotraining/pom.xml', LF will be replaced by CRLF the next time Git touches it
+        //warning: in the working copy of 'Autotraining/src/main/java/Baitapjava/Baitap1.java', LF will be replaced by CRLF the next time Git touches it
 
     }
 }
