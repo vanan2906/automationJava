530be0e4a2cb28555425f7b1275cc666d6610a34
anhntv4 <anhntv4@smartosc.com>
2023-11-17 17:25:16 +0700

    a

---
+++ Autotraining/src/main/java/Baitapjava/Baitap1.java
@@ -0,0 +1,7 @@
+package Baitapjava;
+
+public class Baitap1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Autotraining/src/main/java/Baitapjava/Baitap2.java
@@ -0,0 +1,4 @@
+package Baitapjava;
+
+public class Baitap2 {
+}
