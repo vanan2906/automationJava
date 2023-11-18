fe3403dc393be5de5f3e27f64796e762fdeea10c
AliceNguyen <vananh1998k11@gmail.com>
2023-11-18 17:26:02 +0700

    Hello

--- Autotraining/src/main/java/Baitapjava/Baitap5.java
+++ Autotraining/src/main/java/Baitapjava/Baitap5.java
@@ -1,7 +1,19 @@
 package Baitapjava;
 
+import java.util.Scanner;
+
 public class Baitap5 {
     public static void main(String[] args) {
+        String Masinhvien;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào MSSV:");
+
+        Masinhvien = sc.nextLine();
+// Phải là số khác 0
+        if (Masinhvien.matches("^B[1-9]{7}$"))
+            System.out.println("Mã sinh viên hợp lệ");
+        else
+            System.out.println("Mã sinh viên không hợp lệ");
 
     }
 }
